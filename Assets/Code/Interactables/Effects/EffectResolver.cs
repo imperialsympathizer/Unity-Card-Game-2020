a66ae5492fd37d5119321a9af829c456f3f0c0a0
imperialsympathizer <imperialsynthesizer@gmail.com>
2020-10-19 10:09:14 -0500

    ben/alpha-14: code cleanup

--- Assets/Code/Interactables/Effects/EffectResolver.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class EffectResolver {
-    // This class handles the actual resolution of effects
-    // Where DynamicEffect is the effect and DynamicEffectController is the sequencer,
-    // EffectResolver does the heavy lifting for anything related to what an effect actually does from a game standpoint
-}
